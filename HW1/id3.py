71d949d36725bea33f8609e7a8fe9b33a23f6d5e
Will Frank <willfrank98@gmail.com>
2019-02-08 01:49:13 -0700

    more work

--- HW1/id3.py
+++ HW1/id3.py
@@ -8,78 +8,6 @@ class Node:
     splitsOn = ""
     prediction = ""
 
-    # def __init__(self, label):
-    #     self.label = label
-
-    # def add_child(self, child):
-    #     self.children.append(child)
-
-    # def add_parent(self, parent):
-    #     self.parent = parent
-
-    # def splits_on(self, split):
-    #     self.splitsOn = split
-
-
-def infogain(S, Attributes):
-    """gets the attribute with the best information gain"""
-    entropy = entropycalc(S)
-
-    maxInfo = 0
-    maxAttr = ""
-    for A in Attributes:
-        infoGain = infohelper(S, A, entropy)
-        if infoGain > maxInfo:
-            maxInfo = infoGain
-            maxAttr = A
-
-    return maxAttr
-
-
-def infohelper(S, A, entropy):
-    """calculates the info gain of a specific attribute"""
-    newEnt = 0.0
-    for v in values(S, A):
-        # creates a list of examples where attr A has value v
-        Sv = getSv(S, A, v)
-        ratio = len(Sv)/float(len(S))
-        ent = entropycalc(Sv)
-        newEnt += ratio * ent
-
-    return entropy - newEnt
-
-
-def getSv(S, A, v):
-    """Gets a list of every s in S where s[A] has value v"""
-    return [s for s in S if s[A] == v]
-
-
-def entropycalc(S):
-    """calculates the entropy of S"""
-    labels = {}
-    for s in S:
-        label = s["Label"]
-        if label not in labels:
-            labels[label] = 0
-        labels[label] += 1
-
-    entropy = 0.0
-    norm = len(S)  # normalizing value
-    for (label, quant) in labels.items():
-        ratio = quant/float(norm)
-        entropy -= math.log((ratio), 2) * (ratio)
-
-    return entropy
-
-
-def values(S, A):
-    """gets every value that attribute A takes in S"""
-    vals = set()
-    for exm in S:
-        vals.add(exm[A])
-
-    return vals
-
 
 def ID3(S, Attributes, Label):
     #check if all labels are the same
@@ -97,7 +25,7 @@ def ID3(S, Attributes, Label):
         return leaf
 
     # get attribute A that best splits S
-    A = infogain(S, Attributes)
+    A = MajorityError(S, Attributes)
 
     #create a root node for tree
     root = Node()
@@ -126,16 +54,130 @@ def ID3(S, Attributes, Label):
 
     return root
 
+def getSv(S, A, v):
+    """Gets a list of every s in S where s[A] has value v"""
+    return [s for s in S if s[A] == v]
 
-S = [{"x1": 0, "x2": 0, "x3": 1, "x4": 0, "Label": 0},
-     {"x1": 0, "x2": 1, "x3": 0, "x4": 0, "Label": 0},
-     {"x1": 0, "x2": 0, "x3": 1, "x4": 1, "Label": 1},
-     {"x1": 1, "x2": 0, "x3": 0, "x4": 1, "Label": 1},
-     {"x1": 0, "x2": 1, "x3": 1, "x4": 0, "Label": 0},
-     {"x1": 1, "x2": 1, "x3": 0, "x4": 0, "Label": 0},
-     {"x1": 0, "x2": 1, "x3": 0, "x4": 1, "Label": 0}]
 
-Attributes = ["x1", "x2", "x3", "x4"]
+def values(S, A):
+    """gets every value that attribute A takes in S"""
+    vals = set()
+    for exm in S:
+        vals.add(exm[A])
+
+    return vals
+
+
+def MajorityError(S, Attributes):
+    """gets the attribute with the lowest majority error"""
+    ME = majerrcalc(S)
+
+    maxGain = 0
+    maxAttr = ""
+    for A in Attributes:
+        gain = mehelper(S, A, ME)
+        if gain > maxGain:
+            maxGain = gain
+            maxAttr = A
+
+    return maxAttr
+
+
+def mehelper(S, A, ME):
+    """"""
+    newME = 0.0
+    for v in values(S, A):
+        # creates a list of examples where attr A has value v
+        Sv = getSv(S, A, v)
+        ratio = len(Sv)/float(len(S))
+        me = majerrcalc(Sv)
+        newME += ratio * me
+
+    return ME - newME
+
+def majerrcalc(S):
+    """gets the majority error of S"""
+    labels = {}
+    for s in S:
+        label = s["Label"]
+        if label not in labels:
+            labels[label] = 0
+        labels[label] += 1
+
+    maxLabel = ""
+    numLabel = 0
+    for label, num in labels.items():
+        if num > numLabel:
+            numLabel = num
+            maxLabel = label
+
+    numWrong = 0
+    for s in S:
+        label = s["Label"]
+        if label != maxLabel:
+            numWrong += 1
+
+    return numWrong/float(len(S))
+
+def infogain(S, Attributes):
+    """gets the attribute with the best information gain"""
+    entropy = entropycalc(S)
+
+    maxInfo = 0
+    maxAttr = ""
+    for A in Attributes:
+        infoGain = infohelper(S, A, entropy)
+        if infoGain > maxInfo:
+            maxInfo = infoGain
+            maxAttr = A
+
+    return maxAttr
+
+
+def entropycalc(S):
+    """calculates the entropy of S"""
+    labels = {}
+    for s in S:
+        label = s["Label"]
+        if label not in labels:
+            labels[label] = 0
+        labels[label] += 1
+
+    entropy = 0.0
+    norm = len(S)  # normalizing value
+    for (label, quant) in labels.items():
+        ratio = quant/float(norm)
+        entropy -= math.log((ratio), 2) * (ratio)
+
+    return entropy
+
+
+def infohelper(S, A, entropy):
+    """calculates the info gain of a specific attribute"""
+    newEnt = 0.0
+    for v in values(S, A):
+        # creates a list of examples where attr A has value v
+        Sv = getSv(S, A, v)
+        ratio = len(Sv)/float(len(S))
+        ent = entropycalc(Sv)
+        newEnt += ratio * ent
+
+    return entropy - newEnt
+
+
+file = open("input.txt")
+Attributes = file.readline().strip().split(' ')
+
+S = []
+for line in file:
+    example = {}
+    i = 0
+    for state in line.strip().split(' '):
+        example[Attributes[i]] = state
+        i += 1
+    S.append(example)
+
+Attributes.remove("Label")
 
 Label = "0"
 
