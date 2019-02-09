cce2ecc8e6dd2bea230fdca676a30c1c6b5dc9fc
Will Frank <willfrank98@gmail.com>
2019-02-08 18:05:41 -0700

    done with #1.2

--- HW1/id3.py
+++ HW1/id3.py
@@ -1,15 +1,21 @@
 import math
+from graphviz import Graph
 
 
 class Node:
     children = list()
-    #parent = None
     label = ""
     splitsOn = ""
     prediction = ""
 
+    def __init__(self):
+        self.children = list()
+        self.label = ""
+        self.splitsOn = ""
+        self.prediction = ""
 
-def ID3(S, Attributes, Label):
+
+def ID3(S, Attributes, Label, Split):
     #check if all labels are the same
     labelCheck = S[0]["Label"]
     allSame = True
@@ -25,30 +31,39 @@ def ID3(S, Attributes, Label):
         return leaf
 
     # get attribute A that best splits S
-    A = MajorityError(S, Attributes)
+    A = Split(S, Attributes)
 
     #create a root node for tree
     root = Node()
     root.splitsOn = A
 
-    for v in values(S, A):
-        #temp = Node(v)
-        #root.add_child(temp)
+    for v in Attributes[A]:
         Sv = getSv(S, A, v)
 
         if len(Sv) == 0:
             # add leaf node w/ most common value of Label in S
-            labels = {"": 0}
+            labels = {}
             for s in S:
                 label = s["Label"]
                 if label not in labels:
                     labels[label] = 0
                 labels[label] += 1
+
+            maxNum = 0
+            maxLabel = ""
+            for label, num in labels.items():
+                if num > maxNum:
+                    maxNum = num
+                    maxLabel = label
+            
+            leaf = Node()
+            leaf.prediction = maxLabel
+            leaf.label = v
+            root.children.append(leaf)
         else:
-            tempAttr = list(Attributes)
-            tempAttr.remove(A)
-            subtree = ID3(Sv, tempAttr, Label)
-            #subtree.add_parent(root)
+            tempAttr = dict(Attributes)
+            tempAttr.pop(A)
+            subtree = ID3(Sv, tempAttr, Label, Split)
             subtree.label = v
             root.children.append(subtree)
 
@@ -59,23 +74,19 @@ def getSv(S, A, v):
     return [s for s in S if s[A] == v]
 
 
-def values(S, A):
-    """gets every value that attribute A takes in S"""
-    vals = set()
-    for exm in S:
-        vals.add(exm[A])
-
-    return vals
+def values(Attributes, A):
+    """gets every value that attribute A takes"""
+    return Attributes[A]
 
 
 def MajorityError(S, Attributes):
     """gets the attribute with the lowest majority error"""
-    ME = majerrcalc(S)
+    ME = ME_calc(S)
 
-    maxGain = 0
+    maxGain = -1 # in case purity gain is 0 across the board
     maxAttr = ""
     for A in Attributes:
-        gain = mehelper(S, A, ME)
+        gain = ME_helper(S, Attributes, A, ME)
         if gain > maxGain:
             maxGain = gain
             maxAttr = A
@@ -83,20 +94,23 @@ def MajorityError(S, Attributes):
     return maxAttr
 
 
-def mehelper(S, A, ME):
+def ME_helper(S, Attributes, A, ME):
     """"""
     newME = 0.0
-    for v in values(S, A):
+    for v in Attributes[A]:
         # creates a list of examples where attr A has value v
         Sv = getSv(S, A, v)
         ratio = len(Sv)/float(len(S))
-        me = majerrcalc(Sv)
+        me = ME_calc(Sv)
         newME += ratio * me
 
     return ME - newME
 
-def majerrcalc(S):
+def ME_calc(S):
     """gets the majority error of S"""
+    if len(S) == 0:
+        return 0
+
     labels = {}
     for s in S:
         label = s["Label"]
@@ -119,14 +133,14 @@ def majerrcalc(S):
 
     return numWrong/float(len(S))
 
-def infogain(S, Attributes):
+def InfoGain(S, Attributes):
     """gets the attribute with the best information gain"""
-    entropy = entropycalc(S)
+    entropy = ent_calc(S)
 
     maxInfo = 0
     maxAttr = ""
     for A in Attributes:
-        infoGain = infohelper(S, A, entropy)
+        infoGain = infohelper(S, Attributes, A, entropy)
         if infoGain > maxInfo:
             maxInfo = infoGain
             maxAttr = A
@@ -134,8 +148,24 @@ def infogain(S, Attributes):
     return maxAttr
 
 
-def entropycalc(S):
+def infohelper(S, Attributes, A, entropy):
+    """calculates the info gain of a specific attribute"""
+    newEnt = 0.0
+    for v in Attributes[A]:
+        # creates a list of examples where attr A has value v
+        Sv = getSv(S, A, v)
+        ratio = len(Sv)/float(len(S))
+        ent = ent_calc(Sv)
+        newEnt += ratio * ent
+
+    return entropy - newEnt
+
+
+def ent_calc(S):
     """calculates the entropy of S"""
+    if len(S) == 0:
+        return 0
+
     labels = {}
     for s in S:
         label = s["Label"]
@@ -152,35 +182,114 @@ def entropycalc(S):
     return entropy
 
 
-def infohelper(S, A, entropy):
-    """calculates the info gain of a specific attribute"""
-    newEnt = 0.0
-    for v in values(S, A):
+def GiniIndex(S, Attributes):
+    """gets the attribute with the lowest Gini Index"""
+    initialGini = GI_calc(S)
+    print "Gini Index = " + "{:.2f}".format(initialGini)
+
+    maxInfo = 0
+    maxAttr = ""
+    for A in Attributes:
+        print "GI for " + A + " = "
+        infoGain = ginihelper(S, Attributes, A, initialGini)
+        if infoGain > maxInfo:
+            maxInfo = infoGain
+            maxAttr = A
+
+    return maxAttr
+
+def ginihelper(S, Attributes, A, gini):
+    """calculates the new gini index of splitting on a specific attribute"""
+    newGI = 0.0
+    output = "$" + "{:.2f}".format(gini) + " - ("
+    for v in Attributes[A]:
         # creates a list of examples where attr A has value v
         Sv = getSv(S, A, v)
         ratio = len(Sv)/float(len(S))
-        ent = entropycalc(Sv)
-        newEnt += ratio * ent
+        gi = GI_calc(Sv)
+        output += "\\frac{" + str(len(Sv)) + "}{" + str(len(S)) + "}(" + "{:.2f}".format(gi) + ") + "
+        newGI += ratio * gi
 
-    return entropy - newEnt
+    output += ") \\approx " + "{:.2f}".format(gini - newGI) + "$"
+    print output
+    return gini - newGI
+
+def GI_calc(S):
+    """calculates the Gini Index of S"""
+    if len(S) == 0:
+        return 0
+
+    labels = {}
+    for s in S:
+        label = s["Label"]
+        if label not in labels:
+            labels[label] = 0
+        labels[label] += 1
+
+    gini = 1.0
+    norm = len(S)  # normalizing value
+    for (label, quant) in labels.items():
+        ratio = quant/float(norm)
+        gini -= ratio**2
+
+    return gini
 
 
-file = open("input.txt")
-Attributes = file.readline().strip().split(' ')
+def render(root, name):
+    graph = Graph()
+    nodes = get_nodes(root)
+    edges = get_edges(root)
+
+    for node in nodes:
+        graph.node(node[0], node[1])
+
+    for edge in edges:
+        graph.edge(edge[0], edge[1], edge[2])
+
+    graph.render(name, view=True, format="pdf")
+
+
+def get_nodes(root):
+    nodes = []
+    if root.splitsOn == "":
+        nodes.append((str(id(root)), "Label: " + root.prediction))
+    else:
+        nodes.append((str(id(root)), root.splitsOn))
+    
+    for child in root.children:
+        nodes.extend(get_nodes(child))
+    return nodes
+
+
+def get_edges(root):
+    edges = []
+    for child in root.children:
+        edges.append((str(id(root)), str(id(child)), child.label))
+        edges.extend(get_edges(child))
+    return edges
+
+
+file = open("tennis.txt")
+numAttr = int(file.readline().strip())
+
+listAttr = file.readline().strip().split(' ')
+listAttr.remove("Label")
+Attributes = {}
+for i in range(0, numAttr):
+    Attributes[listAttr[i]] = file.readline().strip().split(' ')
+listAttr.append("Label") # Label isn't an attribute, but we need it to build examples
 
 S = []
 for line in file:
     example = {}
     i = 0
     for state in line.strip().split(' '):
-        example[Attributes[i]] = state
+        example[listAttr[i]] = state
         i += 1
     S.append(example)
 
-Attributes.remove("Label")
-
 Label = "0"
 
-MegaRoot = ID3(S, Attributes, Label)
+MegaRoot = ID3(S, Attributes, Label, GiniIndex)
 
-print("Hello World")
+render(MegaRoot, "treetennis2")
