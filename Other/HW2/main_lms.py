fb331a773dd79db30c0a1f296825cffe42468120
Will Frank <willfrank98@gmail.com>
2019-02-25 16:10:33 -0700

    removed some unused code

--- Other/HW2/main_lms.py
+++ Other/HW2/main_lms.py
@@ -1,20 +1,5 @@
 from lms import Batch_LMS, Stochastic_LMS, get_cost
 
-def calc_median(arr):
-    n = len(arr)
-    if n < 1:
-            return None
-    if n % 2 == 1:
-            return sorted(arr)[n//2]
-    else:
-            return sum(sorted(arr)[n//2-1:n//2+1])/2.0
-
-
-def reset_weights(S):
-    weight = 1/float(len(S))
-    for s in S:
-        s["Weight"] = weight
-
 ### MAIN ###
 dataset = "concrete"
 
