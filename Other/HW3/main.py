ca5b0619c353484b6b8b62f8abe3c3d75da63013
Will Frank <willfrank98@gmail.com>
2019-02-27 14:40:38 -0700

    almost done hw3

---
+++ Other/HW3/main.py
@@ -0,0 +1,54 @@
+from perceptron import *
+
+bank_note = "bank-note/"
+
+f = open(bank_note + "train.csv")
+
+Attributes = ["Variance", "Skewness", "Curtosis", "Entropy", "Label"]
+
+S_train = []
+for line in f:
+    attrs = line.strip().split(',')
+    example = {}
+    for i in range(0, len(Attributes)):
+        if Attributes[i] == "Label":
+            attrs[i] = 1 if attrs[i] == '1' else -1
+        example[Attributes[i]] = attrs[i]
+    S_train.append(example)
+
+f = open(bank_note + "test.csv")
+S_test = []
+for line in f:
+    attrs = line.strip().split(',')
+    example = {}
+    for i in range(0, len(Attributes)):
+        if Attributes[i] == "Label":
+            attrs[i] = 1 if attrs[i] == '1' else -1
+        example[Attributes[i]] = attrs[i]
+    S_test.append(example)
+
+Attributes.remove("Label")
+
+w = Perceptron_Standard(S_train, Attributes, .1, 10)
+err_train = Perceptron_Test(w, S_train, Attributes)
+err_test = Perceptron_Test(w, S_test, Attributes)
+print "w: " + str(w)
+print "Training Error: " + str(err_train)
+print "Testing Error: " + str(err_test)
+
+WC = Perceptron_Voted(S_train, Attributes, .1, 10)
+err_train = Perceptron_Test_Voted(WC, S_train, Attributes)
+err_test = Perceptron_Test_Voted(WC, S_test, Attributes)
+o = open("all_weights.txt", "w")
+o.write("Votes\t\tWeight Vector\n")
+for w, c in zip(WC[0], WC[1]):
+    o.write("votes = " + str(c) + ",\tw = " + str(w) + "\n")
+print "Training Error: " + str(err_train)
+print "Testing Error: " + str(err_test)
+
+w = Perceptron_Average(S_train, Attributes, .1, 10)
+err_train = Perceptron_Test(w, S_train, Attributes)
+err_test = Perceptron_Test(w, S_test, Attributes)
+print "a: " + str(w)
+print "Training Error: " + str(err_train)
+print "Testing Error: " + str(err_test)
