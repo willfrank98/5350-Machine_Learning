d2a461c607b20df1a8821d1e7d9a0b607bf4b84f
Will Frank <willfrank98@gmail.com>
2019-04-24 01:25:33 -0600

    implemented NN with tf/keras?

---
+++ Other/HW5/tf_bonus.py
@@ -0,0 +1,71 @@
+#
+#   Must be run with 64-bit python 3.6.x and tensorflow
+#
+
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+# Helper libraries
+import numpy as np
+# import matplotlib.pyplot as plt
+
+
+## Import data ##
+
+bank_note = "bank-note/"
+
+train_data, train_labels, test_data, test_labels = [], [], [], []
+
+f = open(bank_note + "train.csv")
+for line in f:
+    attrs = line.strip().split(',')
+    train_data.append([float(s) for s in attrs[:-1]])
+    if attrs[-1] == '1':
+        train_labels.append(1)
+    else:
+        train_labels.append(0)
+
+f = open(bank_note + "test.csv")
+for line in f:
+    attrs = line.strip().split(',')
+    test_data.append([float(s) for s in attrs[:-1]])
+    if attrs[-1] == '1':
+        test_labels.append(1)
+    else:
+        test_labels.append(0)
+
+train_data = np.array(train_data)
+train_labels = np.array(train_labels)
+test_data = np.array(test_data)
+test_labels = np.array(test_labels)
+
+f = open('output.txt', 'w')
+
+## construct NN ##
+
+widths = [5, 10, 25, 50, 100]
+depths = [3, 5, 9]
+acts = [keras.activations.tanh, tf.nn.relu]
+inits = [tf.contrib.layers.xavier_initializer(), tf.initializers.he_normal()]
+for i in range(2):
+    print('Act/Int ' + str(i))
+    f.write('Act/Int ' + str(i) + '\n')
+    for w in widths:
+        for d in depths:
+            layers = [keras.layers.Dense(units=w, activation=acts[i], kernel_initializer=inits[i], bias_initializer=inits[i]) for _ in range(d - 1)]
+            layers.append(keras.layers.Dense(units=1, activation=acts[i], kernel_initializer=inits[i], bias_initializer=inits[i]))
+
+            model = keras.Sequential(layers)
+
+            model.compile(optimizer='adam', loss='mean_squared_error', metrics=['accuracy'])
+
+            # Train model
+            model.fit(train_data, train_labels, epochs=10)
+
+            # Evaluate
+            _, test_acc = model.evaluate(test_data, test_labels)
+            _, train_acc = model.evaluate(train_data, train_labels)
+
+            print(str(w) + '/' + str(d) + ': ' + str(train_acc) + ', ' + str(test_acc))
+            f.write(str(w) + '/' + str(d) + ': ' + str(train_acc) + ', ' + str(test_acc) + '\n')
